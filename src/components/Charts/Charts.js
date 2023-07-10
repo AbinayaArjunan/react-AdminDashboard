9c49760a7ec7fe2db37f1a8f615a5ae5d3e27c13
abinayaarjunan <abiharsha2000@gmail.com>
2023-07-10 12:57:01 +0530

    commit

--- src/components/Charts/Charts.js
+++ src/components/Charts/Charts.js
@@ -11,7 +11,7 @@ import {
     Title,
     Filler,
 } from "chart.js";
-import { Doughnut, Line, Scatter } from "react-chartjs-2";
+import { Doughnut, Line } from "react-chartjs-2";
 
 ChartJS.register(
     ArcElement,
@@ -28,14 +28,12 @@ ChartJS.register(
 const DashBoardCharts = () => {
     return (
         <>
-            {/*Pie Chart*/}
+
             <div class="col-lg-8">
                 <div class="card shadow mb-4">
-                    {/*Card Header - Dropdown*/}
                     <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
                         <h6 class="m-0 font-weight-bold text-primary">Earnings Overview</h6>
                     </div>
-                    {/*Card Body*/}
                     <div class="card-body">
                         <Line
                             options={{
@@ -44,10 +42,7 @@ const DashBoardCharts = () => {
                                     legend: {
                                         position: "top",
                                     },
-                                    // title: {
-                                    //     display: true,
-                                    //     text: "Chart.js Line Chart",
-                                    // },
+
                                 },
                             }}
                             data={{
