dc51fca65f04ba6476d6aa6841c7a9d4e4c96628
abinayaarjunan <abiharsha2000@gmail.com>
2023-07-11 14:04:28 +0530

    commit

--- src/components/Dashboard/DashBoard.js
+++ src/components/Dashboard/DashBoard.js
@@ -215,13 +215,13 @@ const DashBoard = () => {
                             </div>
                             <p>
                                 Add some quality, svg illustrations to your project courtesy of
-                                <a target="_blank" rel="noopener" href="https://undraw.co/">
+                                <a  href="https://undraw.co/">
                                     unDraw
                                 </a>
                                 , a constantly updated collection of beautiful svg images that
                                 you can use completely free and without attribution!
                             </p>
-                            <a target="_blank" rel="noopener" href="https://undraw.co/">
+                            <a  href="https://undraw.co/">
                                 Browse Illustrations on unDraw &rarr;
                             </a>
                         </div>
